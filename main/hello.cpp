c1604974094d3128ced3b0eb32bd663335992342
Pajju54 <prajwaljoshi51@gmail.com>
2025-03-13 10:47:45 +0530

    vser

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout<<"Working on this!"<<endl;
+  cout<<"Worked. Running automatically Yayyy!!"<<endl;
   return 0;
 }
